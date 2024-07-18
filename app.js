48fd5f59cda94ce2e30f4f35ac4fd1613e2c8a93
tosik-14 <58371787+tosik-14@users.noreply.github.com>
2024-07-18 23:16:49 +0300

    Add files via upload
    Version 3. Added: Add your items to the game. Fixed: no hardcode. 
    Rock_Paper_Scissors, with the ability to select from 3 to 7 parameters directly in the program. There is a check for input and exit from the program.  Link to video on google drive: https://drive.google.com/drive/folders/1wsCH_t2Gb79UmEmWmP0zUIGUjDhKhnQU?usp=sharing

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+// version 3
+
 const readline = require('readline');
 const crypto = require('crypto');  
 const Table = require('cli-table');
+//const { table } = require('console');
 //const { upgrade } = require('c:/users/tosik-asus/appdata/local/microsoft/typescript/5.2/node_modules/undici-types/api');
+
 
 
 let gameIndex = 3;
@@ -14,48 +18,60 @@ const rl = readline.createInterface({
 
 
 class Help {
-    constructor() {
-        this.arr = ['Fire', 'Water', 'Air', 'Sponge'];
-        this.colWidths = [15, 10, 10, 10];
+    constructor(array) {
+        this.arr = array;
+        this.colWidths = [15];
+        this.resultCl = new Results(this.arr);
+        this.game = new Game(this.arr)
         this.newTable();
-        this.game = new Game();
-        this.results = new Results();
     }
 
     newTable() {
         this.table = new Table({
             colWidths: this.colWidths
         });
-        this.table.push(
-            ['PC / User', 'Rock', 'Scissors', 'Paper'],
-            ['Rock', 'Draw', 'Lose', 'Win'],
-            ['Scissors', 'Win', 'Draw', 'Lose'],
-            ['Paper', 'Lose', 'Win', 'Draw']
-        );
+        for (let i = 1; i < 3; i++) {
+            this.updateTable();
+        }
     }
 
     updateTable() {
-        const newItem = this.arr[this.table.length - 4];
+
+        if (this.table.length < 1) {
+            const buff = [this.arr[this.table.length]];
+            this.table.push(buff);
+            const newItem = this.arr[this.table.length];
+            this.colWidths.push(10);
+            this.table[0].push(newItem);
+            const buf = [this.arr[this.table.length]];
+            this.table.push(buf);
+            for (let i = 1; i < this.table.length; i++) {  // add first item
+                const user = this.table[i][0];
+                const result = this.resultCl.result(this.game.getMoveId(newItem), this.game.getMoveId(user));
+                this.table[i].push(this.getResultText(result));
+            }
+        }
+        
+        const newItem = this.arr[this.table.length];
         this.colWidths.push(10);
 
         this.table[0].push(newItem);
 
-        
+
         for (let i = 1; i < this.table.length; i++) {  // add column
             const user = this.table[i][0];
-            const result = this.results.result(this.game.getMoveId(newItem), this.game.getMoveId(user));
+            const result = this.resultCl.result(this.game.getMoveId(newItem), this.game.getMoveId(user));
             this.table[i].push(this.getResultText(result));
         }
 
         const newRow = [newItem];
         for (let i = 1; i < this.table.length; i++) {    //add row      
             const pc = this.table[0][i];
-            const result = this.results.result(this.game.getMoveId(pc), this.game.getMoveId(newItem));
+            const result = this.resultCl.result(this.game.getMoveId(pc), this.game.getMoveId(newItem)); // почему он блять не видит метод result из касса Rusults???
             newRow.push(this.getResultText(result));
         }
-
-
-        newRow.push('Draw');  
+        
+        newRow.push('Draw');
         this.table.push(newRow);
     }
 
@@ -67,8 +83,8 @@ class Help {
         }
     }
 
-    cleaningTable() {
-        this.colWidths = [15, 10, 10, 10];
+    changeTable() {
+        this.colWidths = [15];
         this.newTable();
         for (let i = 3; i < gameIndex; i++) {
             this.updateTable();
@@ -78,13 +94,16 @@ class Help {
     display() {
         console.log(this.table.toString());
     }
-    
+
 }
 
+
 class Game {
-    constructor(results) {
-        this.pcMoveHash = new PcMoveHash();
-        this.gameResults = results;
+    constructor(arr) {
+        this.items = arr;
+
+        this.pcMoveHash = new PcMoveHash(this.items);
+        this.gameResults = new Results(this.items);
     }
 
     async play() {
@@ -96,58 +115,12 @@ class Game {
         const playerChoice = await this.getPlayerMove();
         let playerMove;
         //if (playerMove != 1 || playerMove != 2 || playerMove != 3) { console.Log("Incorrect move, try again"); return; }
-        switch (playerChoice) {
-            case '1':
-                playerMove = 'Rock';
-                break;
-            case '2':
-                playerMove = 'Scissors';
-                break;
-            case '3':
-                playerMove = 'Paper';
-                break;
-            case '4':
-                if (gameIndex >= 4) {
-                    playerMove = 'Fire';
-                }
-                else {
-                    console.log("Unknown move, try again\n");
-                    return;
-                }
-                break;
-            case '5':
-                if (gameIndex >= 5) {
-                    playerMove = 'Water';
-                }
-                else {
-                    console.log("Unknown move, try again\n");
-                    return;
-                }
-                break;
-            case '6':
-                if (gameIndex >= 6) {
-                    playerMove = 'Air';
-                }
-                else {
-                    console.log("Unknown move, try again\n");
-                    return;
-                }
-                break;
-            case '7':
-                if (gameIndex == 7) {
-                    playerMove = 'Sponge';
-                }
-                else {
-                    console.log("Unknown move, try again\n");
-                    return;
-                }
-                break;
-            default:
-                console.log("Unknown move, try again\n");
-                //gameRunning = false;
-                return;
-                break;
+        if (playerChoice > gameIndex || playerChoice < 1) {
+            console.log("Unknown move, try again\n");
+            return;
         }
+        playerMove = this.items[playerChoice];
+        
         console.log(`You made a move with ${playerMove}\n`)
         console.log(`Computer made a move with ${pcMove}\n`);
         let playerMoveId = this.getMoveId(playerMove);
@@ -167,10 +140,10 @@ class Game {
             output: process.stdout
         });*/
         let quest = 'Select move:\n 1) Rock\n 2) Scissors\n 3) Paper\n';
-        let arr = ['Fire', 'Water', 'Air', 'Sponge'];
+        //let arr = ['Fire', 'Water', 'Air', 'Sponge'];
         if (gameIndex > 3) {
             for (let i = 3; i < gameIndex; i++) {
-                quest += ` ${i+1}) ${arr[i - 3]}\n`;
+                quest += ` ${i+1}) ${this.items[i+1]}\n`;
             }
         } 
         return new Promise(
@@ -185,28 +158,53 @@ class Game {
     }
 
     getMoveId(move) {
-        let id = 0;
-        if (move == 'Rock') { id = 1; }
-        else if (move == 'Scissors') { id = 3; }
-        else if (move == 'Paper') { id = 5; }
-        else if (move == 'Fire') { id = 2; }
-        else if (move == 'Water') { id = 7; }
-        else if (move == 'Air') { id = 6; }
-        else if (move == 'Sponge') { id = 4; }
-        return id;
+        //let id = 1;
+        
+        let { N, G } = this.gameResults.calculate();
+        N = this.items.length - 1;
+        G--;
+        let j = 1, b = 1;
+        for (let i = 1; i <= N; i++) {
+            if (move == this.items[i]) {
+                return j;
+            }
+            j += G;
+            if (j > N) {
+                b++;
+                j = b;
+            }
+        }
+        console.log('return 0;');   // check
+        return 0;
+        
     }
 }
 
 class Results {
-    constructor() { 
+    constructor(arr) {
+        this.items = arr;
+        //this.calculate();
+        //this.N = 0;
+        //this.G = 0;
+    }
+
+    calculate() {
+        let N = (this.items.length - 1);
+        if (N % 2 == 0) { N++; }
+        const G = Math.floor(N / 2);
+        const buf = 0;
+        return {
+            param: buf, N, G     // i have a question, without buf it dont work
+        };
     }
 
     result(playerMoveId, pcMoveId) {
+        const { N, G } = this.calculate();
         if (playerMoveId == pcMoveId) { return 2; }  // draw
-        if ((playerMoveId + 3) > 7 && (pcMoveId - 3) < 1) {   // if max param 7 then +-3, if 9 then +-4, if 11 then +-5 and ect
-            pcMoveId += 7;  // max param 7
+        if ((playerMoveId + G) > N && (pcMoveId - G) < 1) {   // if max param 7 then +-3, if 9 then +-4, if 11 then +-5 and ect
+            pcMoveId += N;  // max param 7
         } 
-        if (pcMoveId > playerMoveId && pcMoveId <= playerMoveId + 3) {
+        if (pcMoveId > playerMoveId && pcMoveId <= playerMoveId + G) {
             return 0;  // win
         }
         else {
@@ -216,12 +214,12 @@ class Results {
 }
 
 class PcMoveHash {
-    constructor() {
-        this.Moves = ['Rock', 'Scissors', 'Paper', 'Fire', 'Water', 'Air', 'Sponge'];
+    constructor(items) {
+        this.moves = items;
     }
 
     getPcMove() {
-        const move = this.Moves[Math.floor(Math.random() * ((gameIndex - 1) - 0 + 1)) + 0]
+        const move = this.moves[Math.floor(Math.random() * (gameIndex - 1 + 1) + 1)]
         const key = crypto.randomBytes(16).toString('HEX');
         const hash = crypto.createHash('SHA256').update(move + key).digest('HEX');
         return {
@@ -232,7 +230,7 @@ class PcMoveHash {
 
 class Menu {
     display() {
-        console.log('Select option:\n 1) Play\n 2) Help\n 3) Add parameter\n 4) Delete parameter\n 0) Exit\n');
+        console.log('Select option:\n 1) Play\n 2) Help\n 3) Add parameter\n 4) Delete parameter\n 5) Add custom parameter\n 0) Exit\n');
     }
 
     getOption() {
@@ -245,13 +243,22 @@ class Menu {
             });
         });
     }
+    addCustomParam() {
+        return new Promise((option) => {
+            rl.question('Enter new item: ', (choice) => {
+                //rl.close();
+                option(choice);
+            });
+        });
+    }
 }
 
 (async () => {
+    const items = ['PC / User', 'Rock', 'Scissors', 'Paper', 'Fire', 'Water', 'Air', 'Sponge'/*, 'car', 'tree'*/];
+
     const menu = new Menu();
-    const help = new Help();
-    const results = new Results();
-    const game = new Game(results);
+    const help = new Help(items);
+    const game = new Game(items);
 
     let a = 0;
 
@@ -270,9 +277,9 @@ class Menu {
                 //help.updateTable();
                 break;
             case '3':
-                if (gameIndex < 7) {
-                    help.updateTable();
+                if (gameIndex < (items.length - 1)) {
                     gameIndex++;
+                    help.changeTable();
                 }
                 else {
                     console.log("\nMax number of parametrs reached\n");
@@ -282,12 +289,18 @@ class Menu {
             case '4':
                 if (gameIndex > 3) {
                     gameIndex--;
-                    help.cleaningTable();
+                    help.changeTable();
                 }
                 else {
                     console.log("\nMin number of parametrs reached\n");
                     break;
                 }
+                break;
+            case '5':
+                const newItem = await menu.addCustomParam();
+                items.push(newItem);
+                //help.updateTable();
+                //gameIndex++;
                 break;
             case '0':
                 console.log("Goodbye!\n");
